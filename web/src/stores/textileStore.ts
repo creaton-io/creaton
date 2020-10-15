d9665c48a6b51d3c0516c92094eb0f4ae26aca5c
Anmol Agarwala <anmaga@microsoft.com>
2020-10-15 06:26:31 +0530

    Sylar217/encryptionstub (#12)
    * added encryption placeholders
    
    * removed added env files

--- web/src/stores/textileStore.ts
+++ web/src/stores/textileStore.ts
@@ -9,9 +9,16 @@ export interface FileMetadata {
   date: string;
 }
 
-export interface EncFile {
-  
-} 
+export interface EncryptedFileMetadata {
+  encryptedFile: FileMetadata;
+  exportedKey: FileMetadata;
+}
+
+interface EncryptedMetadata {
+  file: ArrayBuffer;
+  key: ArrayBuffer;
+  counter: Uint8Array;
+}
 
 export class TextileStore {
   private wallet: WalletStore;
@@ -68,12 +75,12 @@ export class TextileStore {
 
   public async setupAPI(): Promise<void> {
     this.api = await Users.withKeyInfo(this.keyInfo);
+    await this.api.getToken(this.identity);
   }
 
   public async setupMailBox(): Promise<void> {
     await this.api.setupMailbox();
   }
-
 
   public async initBucket(): Promise<void> {
     if (!this.identity || !this.keyInfo) {
@@ -84,7 +91,7 @@ export class TextileStore {
     // Authorize the user and your insecure keys with getToken
     await buckets.getToken(this.identity);
 
-    const buck = await buckets.getOrCreate('creaton', undefined, false);
+    const buck = await buckets.getOrCreate('creaton');
     const privBuck = await buckets.getOrCreate('creaton-keys', undefined, true);
 
     if (!buck.root || !privBuck.root) {
@@ -94,29 +101,128 @@ export class TextileStore {
     this.bucketInfo = {
       bucket: buckets,
       bucketKey: buck.root.key,
-      privBucketKey: privBuck.root.key
+      privBucketKey: privBuck.root.key,
     };
   }
 
-  public async uploadFile(file: File, path: string): Promise<FileMetadata> {
+  public async uploadFile(file: File): Promise<EncryptedFileMetadata> {
     const now = new Date().getTime();
     const fileName = `${file.name}`;
     const uploadName = `${now}_${fileName}`;
-    const location = `${path}${uploadName}`;
+    const fileLocation = `contents/${uploadName}`;
+    const keyLocation = `keys/${uploadName}`;
 
-    const encKey = this.encryptFile(file);
-    const raw = await this.bucketInfo.bucket.pushPath(this.bucketInfo.bucketKey, location, file.stream());
+    const encMetadata = await this.encryptFile(file);
+
+    // TODO: Append the counter to the encrypted file.
+    const rawFile = await this.bucketInfo.bucket.pushPath(
+      this.bucketInfo.bucketKey,
+      fileLocation,
+      this.arrayBufferToBase64(encMetadata.file)
+    );
+
+    const rawKey = await this.bucketInfo.bucket.pushPath(
+      this.bucketInfo.privBucketKey,
+      keyLocation,
+      this.arrayBufferToBase64(encMetadata.key)
+    );
 
     return {
-      cid: raw.path.cid.toString(),
-      name: fileName,
-      path: location,
-      date: now.toString(),
+      encryptedFile: {
+        cid: rawFile.path.cid.toString(),
+        name: fileName,
+        path: fileLocation,
+        date: now.toString(),
+      },
+      exportedKey: {
+        cid: rawKey.path.cid.toString(),
+        name: fileName,
+        path: keyLocation,
+        date: now.toString(),
+      },
     };
   }
- 
-public encryptFile(file: File): EncFile{
-  
-}
 
+  public async encryptFile(file: File): Promise<EncryptedMetadata> {
+    const buf = await file.arrayBuffer();
+    const key = await this.generateKey();
+    const counter = window.crypto.getRandomValues(new Uint8Array(16));
+
+    const encryptedFile = await window.crypto.subtle.encrypt(
+      {
+        name: 'AES-CTR',
+        counter: counter,
+        length: 128,
+      },
+      key,
+      buf
+    );
+
+    const exportedKey = await window.crypto.subtle.exportKey('raw', key);
+
+    return {
+      file: encryptedFile,
+      key: exportedKey,
+      counter: counter,
+    };
+  }
+
+  public async decryptRawFile(content: ArrayBuffer, key: ArrayBuffer, counter: Uint8Array): Promise<ArrayBuffer> {
+    const decryptKey = await this.importKey(key);
+    return await window.crypto.subtle.decrypt(
+      {
+        name: 'AES-CTR',
+        counter: counter,
+        length: 128,
+      },
+      decryptKey,
+      content
+    );
+  }
+
+  public async getEncryptionKeys(path: string): Promise<ArrayBuffer> {
+    const metadata = await this.bucketInfo.bucket.pullPath(this.bucketInfo.bucketKey, path);
+
+    const {value} = await metadata.next();
+    const str = new TextDecoder().decode(value);
+
+    return this.base64ToArrayBuffer(str);
+  }
+
+  private async importKey(key: ArrayBuffer): Promise<CryptoKey> {
+    return await window.crypto.subtle.importKey(
+      'raw',
+      key,
+      {
+        name: 'AES-CTR',
+      },
+      false,
+      ['encrypt', 'decrypt']
+    );
+  }
+
+  private async generateKey() {
+    return await window.crypto.subtle.generateKey(
+      {
+        name: 'AES-CTR',
+        length: 256,
+      },
+      true,
+      ['encrypt', 'decrypt']
+    );
+  }
+
+  private arrayBufferToBase64(buffer: ArrayBuffer) {
+    const bytes = new Uint8Array(buffer);
+    const binary = new TextDecoder().decode(bytes);
+
+    return window.btoa(binary);
+  }
+
+  private base64ToArrayBuffer(base64: string) {
+    const str = window.atob(base64);
+    const bytes = new TextEncoder().encode(str);
+
+    return bytes.buffer;
+  }
 }
