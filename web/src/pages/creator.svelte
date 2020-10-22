fd269fd6bbadcbb0d1582b34ff0a24bfd303bef9
Matthew Masurka <m.masurka@gmail.com>
2020-10-22 13:39:08 -0700

    Connects to creator contract

--- web/src/pages/creator.svelte
+++ web/src/pages/creator.svelte
@@ -1,8 +1,29 @@
 <script lang="ts">
+  import WalletAccess from '../templates/WalletAccess.svelte';
+  import {Contract} from '@ethersproject/contracts';
+  import {contracts} from '../contracts.json';
+  import {wallet, flow, chain} from '../stores/wallet';
+  import { onMount } from 'svelte';
 
-  // creators.fetch();
+  let contractAddress;
 
+  if (typeof window !== 'undefined') {
+    contractAddress = window.location.pathname.split('/')[2];
+
+    onMount(async () => {
+      flow.execute(async () => {
+        const creatorContract = new Contract(
+            contractAddress, 
+            contracts.Creator.abi,
+            wallet.provider
+          )
+        console.log(creatorContract)
+      })
+    })
+  }
 </script>
-
-<section class="py-8 px-4 text-center">
-</section>
+<WalletAccess>
+  <section class="py-8 px-4 text-center">
+    {contractAddress}
+  </section>
+</WalletAccess>
