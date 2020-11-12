51fb1682e0c9da685a3b7e9cb3a692519feda6d0
Alexander <alexander_@hotmail.nl>
2020-11-12 02:19:34 -0800

    made truffle work with rollup builtins plugin

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,7 +1,15 @@
+import builtins from 'rollup-plugin-node-builtins';
+
+const builtinsPlugin = builtins({crypto: false});
+builtinsPlugin.name = 'builtins'; // required, see https://github.com/vitejs/vite/issues/728
+
 module.exports = {
   // minify: false,
   // sourcemap: true,
   optimizeDeps: {
     exclude: ['@textile/hub'], // allow to develop web3w with hot reload
+  },
+  rollupInputOptions: {
+    plugins: [builtinsPlugin],
   },
 };
