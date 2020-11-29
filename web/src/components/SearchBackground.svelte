d13418fc99aa5d1c368bdeea3797413ae87fbefd
lesteven <stevenle2011@gmail.com>
2020-11-28 16:30:23 -0800

    add overlay to background

--- web/src/components/SearchBackground.svelte
+++ web/src/components/SearchBackground.svelte
@@ -8,6 +8,7 @@
       style="
         background-image: url(https://i.redd.it/m87tz1bff1g01.png);
       " />
+    <div class="relative backgroundOverlay w-full h-full bg-black opacity-50" />
   </section>
   <section class="relative bg-gradient-to-r from-indigo-400 to-purple-500 h-2/4">
     <slot />
