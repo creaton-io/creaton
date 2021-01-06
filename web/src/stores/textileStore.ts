8783a2c3a2ab162ed27bf1f1e8986f9a612b9dcf
Sina Faraji <sinraf96@gmail.com>
2021-01-06 18:53:50 -0500

    Test if 127.0.0.1 is secure context

--- web/src/stores/textileStore.ts
+++ web/src/stores/textileStore.ts
@@ -224,7 +224,7 @@ export class TextileStore {
     for (const key in data) {
       form_data.append(key, data[key]);
     }
-    const response = await fetch('https://localhost:5000/encrypt', {method: 'POST', body: form_data});
+    const response = await fetch('http://127.0.0.1:5000/encrypt', {method: 'POST', body: form_data});
     return response.text()
   }
 
