6e23c049386637b093fff920723ecdfea96f90f7
ziggy <zzeidan@Futurpreneur.ca>
2021-04-15 09:48:23 -0400

    Added state to checkbox

--- react-app/src/elements/checkbox.stories.tsx
+++ react-app/src/elements/checkbox.stories.tsx
@@ -6,10 +6,13 @@ export default {
 }
 
 export const Checkbox = () => {
+    const [checked, setChecked] = useState(false);
+
     return (
         <div className="flex max-w-7xl mx-auto justify-between flex-wrap">
             <div className="w-1/2 px-12 mt-8">
-                <Component label="Checkbox" />
+                <div className="mb-4">value: {checked.toString().toUpperCase()}</div>
+                <Component label="Checkbox" checked={checked} onChange={() => setChecked(prev => !prev)} />
             </div>
         </div>
     );
--- react-app/src/elements/checkbox.tsx
+++ react-app/src/elements/checkbox.tsx
@@ -1,10 +1,13 @@
 export enum CheckboxState {}
 
-export const Checkbox = ({ label }: { label: string }) => {
+export const Checkbox = ({ label, checked, onChange }: { label: string, checked: boolean, onChange: (e: any) => void  }) => {
     return (
         <div className="flex items-center">
             <input id="remember_me" name="remember_me" type="checkbox"
-                   className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded" />
+                   className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"
+                   onChange={onChange}
+                   checked={checked}
+            />
                 <label htmlFor="remember_me" className="ml-2 block text-sm text-gray-900">
                     {label}
                 </label>
