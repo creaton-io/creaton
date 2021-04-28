9109d1b8095a68f62a321d74efbd9697e042580b
MohammadAmin Khashkhashi Moghaddam <amin.moghaddamv@gmail.com>
2021-04-28 12:21:19 +0200

    Minor Ui improvements

--- react-app/src/components/card.tsx
+++ react-app/src/components/card.tsx
@@ -5,42 +5,48 @@ import {VideoPlayer} from "../VideoPlayer";
 
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-    className?: string;
-    price?: number;
-    name?: string;
-    description?: string,
-    fileUrl?: string;
-    fileType?: string;
-    avatarUrl?: string;
+  className?: string;
+  price?: number;
+  name?: string;
+  description?: string,
+  fileUrl?: string;
+  fileType?: string;
+  avatarUrl?: string;
+  onLike?: any;
+  isLiked?: boolean;
+  likeCount?: number;
 }
 
-export const Card: FC<ButtonProps> = ({ className,price,name,fileUrl, avatarUrl, fileType, description}) => {
-    const [like, setLike] = useState(false);
-    return (
-        <div className="mb-5">
-            <div className="flex flex-col rounded-2xl border p-8 overflow-hidden">
-           
-              {fileUrl && <div className="flex justify-center flex-shrink-0 my-6">
-                  {fileType === "image" 
-                  ? <img className="w-72 object-fit rounded-xl"
-                     src={fileUrl}
-                     alt=""/>
-                     :  <VideoPlayer url={fileUrl}/>
+export const Card: FC<ButtonProps> = ({className, price, name, fileUrl, avatarUrl, fileType, description, isLiked, onLike, likeCount}) => {
+  return (
+    <div className="mb-5">
+      <div className="flex flex-col rounded-2xl border p-8 overflow-hidden">
+
+        {fileUrl && <div className="flex justify-center flex-shrink-0 my-6">
+          {fileType === "image"
+            ? <img className="w-72 object-fit rounded-xl"
+                   src={fileUrl}
+                   alt=""/>
+            : <VideoPlayer url={fileUrl}/>
     }
               </div>}
               <div className="flex-1 bg-white flex flex-col justify-between">
                     <div className="flex items-center justify-between">
-                    <h4 className="text-lg font-semibold text-gray-900">
-                      {name}
-                    </h4>
-                        <div>
-                            <Icon onClick={() => setLike(!like)} name="heart" className={clsx('cursor-pointer',like ? 'text-red-500' : 'text-grey-dark')} />
-                        </div>
+                      <h4 className="text-lg font-semibold text-gray-900">
+                        {name}
+                      </h4>
+                      <div>
+                        <Icon onClick={onLike} name="heart"
+                              className={clsx('cursor-pointer', isLiked ? 'text-red-500' : 'text-grey-dark')}/>
+                        <span className="ml-2">
+                          {likeCount}
+                          </span>
+                      </div>
                     </div>
 
-                    <p>
-                       {description}
-                    </p>
+                <p className="text-left">
+                  {description}
+                </p>
                   {price && <div>
                         <span className="text-xs">
                             Subscribe for
