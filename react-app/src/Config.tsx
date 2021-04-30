6bedd031a53dcab4a85543cf7bf994474783063f
MohammadAmin Khashkhashi Moghaddam <amin.moghaddamv@gmail.com>
2021-04-30 12:33:02 +0200

    Update config uris

--- react-app/src/Config.tsx
+++ react-app/src/Config.tsx
@@ -1,17 +1,18 @@
-let APOLLO_URI, REENCRYPTION_URI, ARWEAVE_URI
+let APOLLO_URI, REENCRYPTION_URI
 const ARWEAVE_GATEWAY = 'https://arweave.net/';
 const REPORT_URI = 'https://report.creaton.io/report'
-
+const ARWEAVE_URI = 'https://arweave.creaton.io'
 if (process.env.NODE_ENV === 'development') {
   REENCRYPTION_URI = 'http://localhost:3010'
   APOLLO_URI = 'http://twitter.creaton.io:8000/subgraphs/name/creaton-io/creaton'
-  // ARWEAVE_URI = 'http://localhost:1984'
-  ARWEAVE_URI = 'https://arweave.creaton.io'
+} else {//staging
+  REENCRYPTION_URI = 'https://staging.creaton.io'
+  APOLLO_URI = 'https://api.thegraph.com/subgraphs/name/creaton-io/creaton-goerli'
 }
-else {
-  REENCRYPTION_URI = 'https://reencryption.creaton.io'
-  APOLLO_URI = 'https://api.thegraph.com/subgraphs/name/creaton-io/creaton2'
-  ARWEAVE_URI = 'https://arweave.creaton.io'
-}
+//TODO:PRODUCTION CONFIG
+// {
+//   REENCRYPTION_URI = 'https://reencryption.creaton.io'
+//   APOLLO_URI = 'https://api.thegraph.com/subgraphs/name/creaton-io/creaton'
+// }
 
 export {APOLLO_URI, ARWEAVE_URI, ARWEAVE_GATEWAY, REENCRYPTION_URI, REPORT_URI}
