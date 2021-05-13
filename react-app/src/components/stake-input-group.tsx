3ae87f58044039bf6a29105bc5a44a78473b08b7
MohammadAmin Khashkhashi Moghaddam <amin.moghaddamv@gmail.com>
2021-05-13 17:34:11 +0200

    Redesign of staking page

--- react-app/src/components/stake-input-group.tsx
+++ react-app/src/components/stake-input-group.tsx
@@ -4,16 +4,15 @@ import {useState} from "react";
 
 
 
-export default function StakeInputGroup({ label, max, buttonLabel, setStakeAmount }) {
-    const [value, setValue] = useState(0)
-
+export default function StakeInputGroup({ label, max, buttonLabel,amount, setStakeAmount, stakeAction }) {
     return (
+      <div className="m-2 p-2 border-2 rounded-md  border-white border-opacity-10">
         <div className="w-full mx-auto">
             <div className="flex justify-between">
-                <label htmlFor="email" className="block text-sm font-medium text-gray-700 font-semibold">
+                <label htmlFor="email" className="block text-sm font-medium text-white font-semibold">
                     {label}
                 </label>
-                <button type="button" className="block text-sm font-medium text-primary font-semibold" onClick={() => setValue(max)}>Max</button>
+                <button type="button" className="block text-sm font-medium text-white font-semibold" onClick={() => setStakeAmount(max)}>Max</button>
             </div>
             <div className="mt-1 flex rounded-md shadow-sm">
                 <div className="relative flex items-stretch flex-grow focus-within:z-10">
@@ -21,11 +20,11 @@ export default function StakeInputGroup({ label, max, buttonLabel, setStakeAmoun
                         type="number"
                         name="number"
                         id="email"
-                        className="focus:ring-indigo-500 focus:border-indigo-500 block w-full rounded-none rounded-l-md sm:text-sm border-gray-300 px-2"
+                        className="focus:ring-indigo-500 focus:outline-none focus:border-indigo-500 block w-full rounded-none rounded-l-md sm:text-sm border-gray-300 px-2 bg-white bg-opacity-20"
                         min={0}
                         max={max}
-                        value={value}
-                        onChange={(e) => setValue(parseInt(setStakeAmount) || 0)}
+                        value={amount}
+                        onChange={(event) => setStakeAmount(parseInt(event.target.value))}
                     />
                 </div>
                 <button
@@ -33,16 +32,18 @@ export default function StakeInputGroup({ label, max, buttonLabel, setStakeAmoun
                     className={clsx(
                         "inline-flex items-center px-8 py-4",
                         "font-medium rounded-r-md leading-4",
-                        "bg-blue-primary text-white",
-                        "hover:bg-blue",
-                        "active:bg-blue-dark",
-                        "focus:outline-none focus:bg-blue focus:ring-1 focus:ring-blue focus:ring-offset-2",
+                        "bg-white bg-opacity-10 text-white",
+                        "hover:bg-opacity-5",
+                        "active:bg-opacity-5",
+                        "focus:outline-none focus:bg-opacity-5 focus:ring-1 focus:ring-white focus:ring-offset-2",
                         "disabled:bg-grey disabled:text-grey-dark",
                     )}
+                    onClick={stakeAction}
                 >
                     {buttonLabel}
                 </button>
             </div>
         </div>
+      </div>
     )
 }
