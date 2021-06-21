4302d26598b109eb28672ac06f80546441716ac7
Aeroxander <alexanderpklus@gmail.com>
2021-06-21 10:28:03 -0500

    whitelist

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -371,7 +371,7 @@ const App = () => {
 
                     <div className="flex-initial">
 
-                      <div className="relative bg-primary-gradiant">
+                      <div className="relative bg-primary-gradient">
                         <div className="hidden sm:block sm:absolute sm:inset-y-0 sm:h-full sm:w-full"
                              aria-hidden="true">
                           <div className="relative h-full max-w-7xl mx-auto"></div>
