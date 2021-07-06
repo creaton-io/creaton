9a09b5b08d694335a2376c0c1e1729fd9bb59a0a
Pyro Hedgehog <jpwilmsmeyer@gmail.com>
2021-07-06 19:29:40 -0400

    Create Testing Token
    Just a quick testing token so that we can mint, and test the FanService items.

---
+++ contracts/test/TestingToken.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TestingToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("Testing Token", "TT") {
+        _mint(msg.sender, initialSupply);
+    }
+
+    function faucet() public {
+        _mint(msg.sender, 10000000000000000000);
+    }
+}
