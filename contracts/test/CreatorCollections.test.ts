8a873cf5445a2806155a560ddb2a8d4b718a5377
SanchitGogia <frl.sanchit@gmail.com>
2021-07-09 19:20:08 -0400

    Update CreatorCollections.test.ts

--- contracts/test/CreatorCollections.test.ts
+++ contracts/test/CreatorCollections.test.ts
@@ -2,6 +2,8 @@ import { expect } from "./chai-setup";
 import { ethers,deployments } from "hardhat";
 import { Contract } from "@ethersproject/contracts";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { ContractFactory } from "ethers";
+import { Console } from "node:console";
 
 
 let FCA: SignerWithAddress;
@@ -11,8 +13,9 @@ let FCtoken;
 let FCtoken2;
 let hardhatToken: Contract;
 let hht: Contract;
-
-
+let TestingToken: ContractFactory;
+let token2: Contract;
+let id;
 describe('FanCollectible tokens', function(){
     beforeEach(async function(){
         [FCA,FAC2,FAC3] = await ethers.getSigners();
@@ -28,19 +31,37 @@ describe('FanCollectible tokens', function(){
         const Tid = await hardhatToken.create(10,5,'Pyrocoin',"0x00");
         const lol2 = (await hardhatToken.getCurrentTokenID()).toString();
         //console.log(Tid);
-        expect(lol1).to.equal(lol2);
+        expect(lol1).to.not.equal(lol2);
     });
 });
-describe('TestingToken', function(){
+describe('CreatorCollections', function(){
     beforeEach(async function(){
-        const TestingToken = await ethers.getContractFactory('TestingToken');
-        const token2 = await TestingToken.deploy(6);
+        TestingToken = await ethers.getContractFactory('TestingToken');
+        token2 = await TestingToken.deploy(6);
         FCtoken2 = await ethers.getContractFactory('CreatorCollections');
-        hht = await FCtoken2.deploy(FCA.address,FCA.address,FAC3.address);
+        hht = await FCtoken2.deploy(FCA.address,hardhatToken.owner(),FCA.address);
 
     });
     it('Check initial supply', async function(){
         expect (await hht.totalSupply()).to.equal(0);
+    });
+    it('Create a pool', async function(){
+        id = (await hht.createPool(1,Math.floor(Date.now() / 1000),ethers.utils.parseEther("10000"),FCA.address, "My first collection"));
+       
+        const poolsArray = await hht.getPoolsForArtist(FCA.address);
+        console.log(poolsArray);
+        expect (await poolsArray.length).to.greaterThan(0);
+    });
+    it ('Testing Create Card', async function(){
+        expect(await hardhatToken.owner()).to.equal(FCA.address);
+        id = (await hht.createPool(1,Math.floor(Date.now() / 1000),ethers.utils.parseEther("10000"),FCA.address, "My first collection"));
+        const poolsArray = await hht.getPoolsForArtist(FCA.address);
+        console.log(poolsArray);
+        //expect (await FCA.address).to.equal(hht.collectible);
+        const TokenId = await hht.createCard(1,ethers.utils.parseEther("10000"),10,20,Math.floor(Date.now() / 1000));
+    const cArray = await  hht.getCardsArray(1);
+    console.log(cArray);
+    expect (await cArray.length).to.greaterThan(0);
     });
 });
 
