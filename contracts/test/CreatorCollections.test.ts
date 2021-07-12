266d8a4afb680bc1d95d46482a812bde0a0435fd
SanchitGogia <frl.sanchit@gmail.com>
2021-07-12 18:20:12 -0400

    Renamed variables
    Pyro asked to rename some variables for testing

--- contracts/test/CreatorCollections.test.ts
+++ contracts/test/CreatorCollections.test.ts
@@ -6,61 +6,61 @@ import { ContractFactory } from "ethers";
 import { Console } from "node:console";
 
 
-let FCA: SignerWithAddress; //rename
-let FAC2: SignerWithAddress; //what are these
-let FAC3: SignerWithAddress; //names
-let FCtoken; //they mean nothing
-let FCtoken2; //to me
-let hardhatToken: Contract; //and i have to mess with this
-let hht: Contract; //what is the difference between this and ^
+let OwnerAccount: SignerWithAddress; //rename
+let TestingAccount1: SignerWithAddress; //what are these
+let TestingAccount2: SignerWithAddress; //names
+let CollectibleFactory; //they mean nothing
+let CollectionsFactory; //to me
+let CollectibleContract: Contract; //and i have to mess with this
+let CollectionsContract: Contract; //what is the difference between this and ^
 let TestingToken: ContractFactory; //this one genuenly makes sense
-let token2: Contract; // and then???
+let TestingTokenContract: Contract; // and then???
 let id;
 describe('FanCollectible tokens', function(){
     beforeEach(async function(){
-        [FCA,FAC2,FAC3] = await ethers.getSigners();
-        FCtoken = await ethers.getContractFactory('FanCollectible');
-        hardhatToken = await FCtoken.deploy('Pyrocoin');
+        [OwnerAccount,TestingAccount1,TestingAccount2] = await ethers.getSigners();
+        CollectibleFactory = await ethers.getContractFactory('FanCollectible');
+        CollectibleContract = await CollectibleFactory.deploy('Pyrocoin');
     });
     it('Check minter\'s address', async function(){
-        // hardhatToken.transferMinter(FCA.address);
-        expect(await hardhatToken.owner()).to.equal(FCA.address);
+         CollectibleContract.transferMinter(OwnerAccount.address);
+        expect(await CollectibleContract.owner()).to.equal(OwnerAccount.address);
     });
     it('Check if create works', async function(){
-        hardhatToken.transferMinter(FCA.address);
-        const lol1 = (await hardhatToken.getCurrentTokenID()).toString();
-        const Tid = await hardhatToken.create(1,'Pyrocoin',"0x00");
-        const lol2 = (await hardhatToken.getCurrentTokenID()).toString();
+        CollectibleContract.transferMinter(OwnerAccount.address);
+        const initialId = (await CollectibleContract.getCurrentTokenID()).toString();
+        const TokenID = await CollectibleContract.create(1,'Pyrocoin',"0x00");
+        const finalId = (await CollectibleContract.getCurrentTokenID()).toString();
         //console.log(Tid);
-        expect(lol1).to.not.equal(lol2);
+        expect(initialId).to.not.equal(finalId);
     });
 });
 describe('CreatorCollections', function(){
     beforeEach(async function(){
         TestingToken = await ethers.getContractFactory('TestingToken');
-        token2 = await TestingToken.deploy(6);
-        FCtoken2 = await ethers.getContractFactory('CreatorCollections');
-        hht = await FCtoken2.deploy(FCA.address,hardhatToken.owner(),FCA.address);
+        TestingTokenContract = await TestingToken.deploy(6);
+        CollectionsFactory = await ethers.getContractFactory('CreatorCollections');
+        CollectionsContract = await CollectionsFactory.deploy(OwnerAccount.address,CollectibleContract.owner(),OwnerAccount.address);
 
     });
     it('Check initial supply', async function(){
-        expect (await hht.totalSupply()).to.equal(0);
+        expect (await CollectionsContract.totalSupply()).to.equal(0);
     });
     it('Create a pool', async function(){
-        id = (await hht.createPool(1,Math.floor(Date.now() / 1000),ethers.utils.parseEther("10000"),FCA.address, "My first collection"));
+        id = (await CollectionsContract.createPool(1,Math.floor(Date.now() / 1000),ethers.utils.parseEther("10000"),OwnerAccount.address, "My first collection"));
        
-        const poolsArray = await hht.getPoolsForArtist(FCA.address);
+        const poolsArray = await CollectionsContract.getPoolsForArtist(OwnerAccount.address);
         console.log(poolsArray);
         expect (await poolsArray.length).to.greaterThan(0);
     });
     it ('Testing Create Card', async function(){
-        expect(await hardhatToken.owner()).to.equal(FCA.address);
-        id = (await hht.createPool(1,Math.floor(Date.now() / 1000),ethers.utils.parseEther("10000"),FCA.address, "My first collection"));
-        const poolsArray = await hht.getPoolsForArtist(FCA.address);
+        expect(await CollectibleContract.owner()).to.equal(OwnerAccount.address);
+        id = (await CollectionsContract.createPool(1,Math.floor(Date.now() / 1000),ethers.utils.parseEther("10000"),OwnerAccount.address, "My first collection"));
+        const poolsArray = await CollectionsContract.getPoolsForArtist(OwnerAccount.address);
         console.log(poolsArray);
-        //expect (await FCA.address).to.equal(hht.collectible);
-        const TokenId = await hht.createCard(1,10,ethers.utils.parseEther("10000"),Math.floor(Date.now() / 1000));
-        const cArray = await  hht.getCardsArray(1);
+        //expect (await OwnerAccount.address).to.equal(CollectionsContract.collectible);
+        const TokenId = await CollectionsContract.createCard(1,10,ethers.utils.parseEther("10000"),Math.floor(Date.now() / 1000));
+        const cArray = await  CollectionsContract.getCardsArray(1);
         console.log(cArray);
         expect (await cArray.length).to.greaterThan(0);
     });
