19e9883fed5df915d9977df2b4a2e76bba0ec3cf
Jagveer-Sangha <jagveersangha@gmail.com>
2021-08-28 11:04:57 -0400

    Update VotingSystem.test.ts

--- contracts/test/VotingSystem.test.ts
+++ contracts/test/VotingSystem.test.ts
@@ -32,22 +32,84 @@ describe('Deploying coin', function () {
 
 describe('Vote with coin', function () {
     let testingToken: Contract;
-
+    let num: number;
     beforeEach(async function () {
         let testingTokenFactory = await ethers.getContractFactory('TestingToken');
         testingToken = await testingTokenFactory.deploy(6);
         // let testingTokenAddress = testingToken.address();
+
 
         [ownerAccount, testingAccount1, testingAccount2] = await ethers.getSigners();
         collectibleFactory = await ethers.getContractFactory('VoteCreators');
         collectibleContract = await collectibleFactory.deploy();
         await collectibleContract.initialize(testingToken.address);
     });
+
     it('Vote with 1 coin', async function () {
         await testingToken.connect(testingAccount1).faucet();
         expect(await testingToken.balanceOf(testingAccount1.address) > 0);
 
-        console.log('works');
+        console.log(await testingToken.balanceOf(testingAccount1.address));
+        // expect(true);//Temp test case
+
+    })
+
+});
+
+
+describe('Vote with no coin', function () {
+    let testingToken: Contract;
+
+    beforeEach(async function () {
+        let testingTokenFactory = await ethers.getContractFactory('TestingToken');
+        testingToken = await testingTokenFactory.deploy(0);
+        // let testingTokenAddress = testingToken.address();
+
+
+        [ownerAccount, testingAccount1, testingAccount2] = await ethers.getSigners();
+        collectibleFactory = await ethers.getContractFactory('VoteCreators');
+        collectibleContract = await collectibleFactory.deploy();
+        await collectibleContract.initialize(testingToken.address);
+    });
+
+    it('Try to vote with no coins', async function () {
+        // expect(await testingToken.totalSupply()).to.equal(0);
+        await testingToken.connect(testingAccount2).faucet();
+
+        expect(await testingToken.balanceOf(testingAccount2.address) > 0); //For some reason this is true
+
+        console.log(await testingToken.balanceOf(testingAccount2.address));
+
+        console.log(await testingToken.balanceOf(testingAccount1.address));
+        console.log((await testingAccount2.getBalance()).toString());
+        console.log(await testingToken.value);
+
+
+        // expect(true);//Temp test case
+    })
+});
+
+describe('Unvote', function () {
+    let testingToken: Contract;
+
+    beforeEach(async function () {
+        let testingTokenFactory = await ethers.getContractFactory('TestingToken');
+        testingToken = await testingTokenFactory.deploy(6);
+        // let testingTokenAddress = testingToken.address();
+
+
+        [ownerAccount, testingAccount1, testingAccount2] = await ethers.getSigners();
+        collectibleFactory = await ethers.getContractFactory('VoteCreators');
+        collectibleContract = await collectibleFactory.deploy();
+        await collectibleContract.initialize(testingToken.address);
+    });
+
+    it('unvote 1 coin', async function () {
+        await testingToken.connect(testingAccount1).faucet();
+        console.log(await testingToken.balanceOf(testingAccount1.address));
+
+        expect(testingToken.balanceOf(testingAccount1.address) - 1);
+        console.log(await testingToken.balanceOf(testingAccount1.address) - 1);
         // expect(true);//Temp test case
     })
 });
