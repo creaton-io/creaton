5a886718132a02fc13d5406475c0678079dc9dcb
Aeroxander <alexanderpklus@gmail.com>
2021-09-30 13:50:07 -0700

    removed web3provider from creators.tsx

--- react-app/src/Creators.tsx
+++ react-app/src/Creators.tsx
@@ -28,24 +28,24 @@ const CREATORS_QUERY = gql`
 
 function Creators() {
   const {loading, error, data} = useQuery(CREATORS_QUERY, {pollInterval: 10000});
-  const {account} = useWeb3React<Web3Provider>()
+  //const {account} = useWeb3React<Web3Provider>()
   if (loading) return (<p>Loading...</p>);
   if (error) return (<p>Error :(</p>);
   const items = data.creators.map((creator: any) => {
-    let subtitle = '$' + creator.subscriptionPrice + ' / month'
-    if(account){
-      const found = creator.subscribers.find(element => element.user.toLowerCase() === account.toLowerCase());
-      if(found) {
-        if(found.status==='requested_subscribe')
-          subtitle="Subscription Requested"
-        if(found.status==='pending_subscribe')
-          subtitle="Access Granted. Start streaming for " + subtitle
-      }
-    }
+    // let subtitle = '$' + creator.subscriptionPrice + ' / month'
+    // if(account){
+    //   const found = creator.subscribers.find(element => element.user.toLowerCase() === account.toLowerCase());
+    //   if(found) {
+    //     if(found.status==='requested_subscribe')
+    //       subtitle="Subscription Requested"
+    //     if(found.status==='pending_subscribe')
+    //       subtitle="Access Granted. Start streaming for " + subtitle
+    //   }
+    // }
     return {
       avatar: creator.profile !== null ? JSON.parse(creator.profile.data).image : "",
       title: creator.profile !== null ? JSON.parse(creator.profile.data).username : creator.id,
-      subtitle: subtitle,
+      //subtitle: subtitle,
       description: creator.description,
       count: creator.subscribers.length,
       source: 'subscribers',
