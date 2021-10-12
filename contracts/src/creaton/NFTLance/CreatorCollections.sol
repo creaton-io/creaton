a28ada9ab6eb927f81df5fa41f4dc187a759c6e3
Pyro Hedgehog <jpwilmsmeyer@gmail.com>
2021-10-12 16:21:41 -0400

    Optimising and renaming (#46)
    * new name for Fan service to NFTLance
    
    renaming and updating the test code
    
    * I messed up the renaming...
    
    * ok, this time for real! we have the name set!
    
    * removed pool start, now each pool is instantly open, (just hide it if it doesnt have any cards)
    
    * Pools Are Now Catalogs!
    
    * renaming of logs
    
    just clean up

--- contracts/src/creaton/NFTLance/CreatorCollections.sol
+++ contracts/src/creaton/NFTLance/CreatorCollections.sol
@@ -3,7 +3,7 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
-// import "hardhat/console.sol";
+import "hardhat/console.sol";
 import "./FanCollectible.sol";
 
 contract CreatorCollections is Ownable, Pausable {
@@ -17,12 +17,11 @@ contract CreatorCollections is Ownable, Pausable {
     FanCollectible private collectible;
 
     uint256 private _totalSupply;
-    mapping(address => uint256[]) private accountToPools; // this is just a nicer way of keep track of who owns what pools.
 
     mapping(uint256 => HoldingTokens) private heldBalances; //tokenID => quantity being held.
     struct HoldingTokens {
         uint256 quantityHeld;
-        uint256 pool;
+        uint256 catalog;
     }
     struct Card {
         uint256[] ids; //Card IDs. would be singular, but each one needs to be unique.
@@ -32,182 +31,145 @@ contract CreatorCollections is Ownable, Pausable {
         uint256 idPointOfNextEmpty;
     }
 
-    struct Pool {
-        uint256 periodStart; // When the collection launches and starts accepting staking tokens.
+    struct Catalog {
         uint256 feesCollected; // Tally of eth collected from cards that require an additional $ to be minted
         address artist;
         string title;
-        uint256 cardsInPool;
+        uint256 cardsInCatalog;
         Card[] cardsArray;
     }
 
-    address public controller;
     mapping(address => uint256) public pendingWithdrawals;
-    mapping(uint256 => Pool) public pools;
-    uint256 public poolsCount;
+    mapping(uint256 => Catalog) public catalogs;
+    uint256 public catalogsCount;
 
-    event UpdatedArtist(uint256 poolId, address artist);
-    event PoolAdded(uint256 poolId, address artist, uint256 periodStart);
-    event CardAdded(uint256 poolId, uint256[] cardIds, uint256 price, uint256 releaseTime);
-    event Staked(address indexed user, uint256 poolId, uint256 amount);
-    event Withdrawn(address indexed user, uint256 poolId, uint256 amount);
-    event Transferred(address indexed user, uint256 fromPoolId, uint256 toPoolId, uint256 amount);
-    event Redeemed(address indexed user, uint256 poolId, uint256 amount);
+    event UpdatedArtist(uint256 catalogId, address artist);
+    event CatalogAdded(uint256 catalogId, address artist, uint256 periodStart);
+    event CardAdded(uint256 catalogId, uint256[] cardIds, uint256 price, uint256 releaseTime);
 
-    modifier poolExists(uint256 id) {
-        require(pools[id].periodStart > 0, "pool does not exists");
+    event Redeemed(address indexed user, uint256 catalogId, uint256 amount);
+
+    modifier catalogExists(uint256 id) {
+        require(catalogs[id].artist != address(0), "catalog does not exists");
         _;
     }
 
-    modifier cardExists(uint256 pool, uint256 card) {
-        require(card < pools[pool].cardsArray.length, "card may not exist");
+    modifier cardExists(uint256 catalog, uint256 card) {
+        require(card < catalogs[catalog].cardsArray.length, "card may not exist");
         _;
     }
 
-    modifier onlyOwnerOrArtist(uint256 pool) {
+    modifier onlyOwnerOrArtist(uint256 catalog) {
         require(
-            pools[pool].artist == _msgSender() || _msgSender() == owner(),
+            catalogs[catalog].artist == _msgSender() || _msgSender() == owner(),
             "You Do Not Have Authorization To Change This"
         );
         _;
     }
 
     constructor(
-        address _controller,
         FanCollectible _collectibleAddress,
         IERC20 _tokenAddress
     ) {
-        controller = _controller;
         collectible = _collectibleAddress;
         token = IERC20(_tokenAddress);
     }
-
-    function purchase(uint256 _poolID, uint256 _cardID)
+    
+    function purchase(uint256 _catalogID, uint256 _cardID)
         public
         whenNotPaused
-        cardExists(_poolID, _cardID)
+        cardExists(_catalogID, _cardID)
         returns (uint256)
     {
-        Pool storage p = pools[_poolID];
+        Catalog storage p = catalogs[_catalogID];
         Card memory c = p.cardsArray[_cardID];
-        require(block.timestamp >= p.periodStart, "pool not open");
+        require(block.timestamp >= c.releaseTime, "card not open");
 
-        require(c.idPointOfNextEmpty < c.ids.length, "Token Is Sold Out");
+        require(c.idPointOfNextEmpty < c.ids.length, "Card Is Sold Out");
 
-        uint256 moreToTake = c.price;
-        _totalSupply = _totalSupply.add(moreToTake);
+        _totalSupply = _totalSupply.add(c.price);
 
-        token.transferFrom(_msgSender(), address(this), moreToTake);
+        token.transferFrom(_msgSender(), address(this), c.price);
 
         p.feesCollected = p.feesCollected.add(c.price);
         // console.log(c.ids[c.idPointOfNextEmpty]);
+        
         collectible.mint(_msgSender(), c.ids[c.idPointOfNextEmpty], "");
         heldBalances[c.ids[c.idPointOfNextEmpty]].quantityHeld = c.price;
-        heldBalances[c.ids[c.idPointOfNextEmpty]].pool = _poolID;
+        heldBalances[c.ids[c.idPointOfNextEmpty]].catalog = _catalogID;
         c.idPointOfNextEmpty++;
-        emit Redeemed(_msgSender(), _poolID, c.price);
+        emit Redeemed(_msgSender(), _catalogID, c.price);
         return c.ids[c.idPointOfNextEmpty - 1];
     }
 
-
     /**
-     * @dev set the artist for the given pool.
-     * @param pool the pool you are setting an artist for
-     * @param artist the address of the artist.
-     */
-    function setArtist(uint256 pool, address artist) public onlyOwnerOrArtist(pool) {
-        uint256 amount = pendingWithdrawals[pools[pool].artist];
-        pendingWithdrawals[pools[pool].artist] = 0;
-        pendingWithdrawals[artist] = pendingWithdrawals[artist].add(amount);
-        pools[pool].artist = artist;
-
-        // this removes the existing artist reference to this... but seems like it will be expensive...
-        for (uint8 x = 0; x < accountToPools[pools[pool].artist].length - 1; x++) {
-            if (accountToPools[pools[pool].artist][x] == pool) {
-                delete accountToPools[pools[pool].artist][x];
-            }
-        }
-        accountToPools[artist].push(pool);
-
-        emit UpdatedArtist(pool, artist);
-    }
-
-    function setController(address _controller) public onlyOwner {
-        uint256 amount = pendingWithdrawals[controller];
-        pendingWithdrawals[controller] = 0;
-        pendingWithdrawals[_controller] = pendingWithdrawals[_controller].add(amount);
-        controller = _controller;
-    }
-
-    /**
-     * @dev creates a card (inside a FanCollectible) for the given pool
-     * @param pool the pool id to add it to
+     * @dev creates a card (inside a FanCollectible) for the given catalog
+     * @param catalog the catalog id to add it to
      * @param supply the supply of these to be made
      * @param price the cost of each item in price
      * @param releaseTime the time you can start buying these
      */
     function createCard(
-        uint256 pool,
+        uint256 catalog,
         uint256 supply,
         uint256 price,
         uint256 releaseTime
-    ) public onlyOwnerOrArtist(pool) poolExists(pool) returns (uint256) {
+    ) public onlyOwnerOrArtist(catalog) catalogExists(catalog) returns (uint256) {
         uint256[] memory tokenIdsGenerated = new uint256[](supply);
         for (uint256 x = 0; x < supply; x++) {
             tokenIdsGenerated[x] = collectible.create("", ""); //URI and Data seem important... and most likely are! well! HAVE FUN!
             //so this generates all the token IDs that will be used, and makes each one unique.
         }
-        pools[pool].cardsArray.push(Card(tokenIdsGenerated, price, releaseTime, 0));
+        catalogs[catalog].cardsArray.push(Card(tokenIdsGenerated, price, releaseTime, 0));
 
-        pools[pool].cardsInPool++;
-        return pools[pool].cardsInPool - 1;
+        catalogs[catalog].cardsInCatalog++;
     }
 
     /**
-    @dev creates a pool.
-    @param id the id of the pool. Must be unique.
-    @param periodStart the time you can start buying these
-    @param title the title of the pool
+    @dev creates a catalog.
+    @param id the id of the catalog. Must be unique.
+    @param title the title of the catalog
     */
-    function createPool(
+    function createCatalog(
         uint256 id,
-        uint256 periodStart,
         string memory title
     ) public returns (uint256) {
-        require(pools[id].periodStart == 0, "pool exists");
+        //TODO: find a better way to check if a catalog is active.
+        require(catalogs[id].artist == address(0), "catalog exists");
 
-        Pool storage p = pools[id];
+        Catalog storage p = catalogs[id];
 
-        p.periodStart = periodStart;
         p.artist = _msgSender();
         p.title = title;
 
-        poolsCount++;
+        catalogsCount++;
 
-        accountToPools[_msgSender()].push(id);
-        emit PoolAdded(id, _msgSender(), periodStart);
+        emit CatalogAdded(id, _msgSender(), block.timestamp);
         return id;
     }
 
-    function cardReleaseTime(uint256 pool, uint256 card) public view returns (uint256) {
-        return pools[pool].cardsArray[card].releaseTime;
+    function cardReleaseTime(uint256 catalog, uint256 card) public view returns (uint256) {
+        return catalogs[catalog].cardsArray[card].releaseTime;
     }
 
     /**
-     * @dev calculates the total suply of tokens that are being staked in this contract
+     * @dev calculates the total supply of tokens that are being staked in this contract
      */
     function totalSupply() public view returns (uint256) {
         return _totalSupply;
     }
 
-    function cardsInPool(uint256 id) public view returns (uint256) {
-        return pools[id].cardsInPool;
+    function cardsInCatalog(uint256 id) public view returns (uint256) {
+        return catalogs[id].cardsInCatalog;
     }
 
     function getCardsArray(uint256 id) public view returns (Card[] memory) {
-        return pools[id].cardsArray;
+        return catalogs[id].cardsArray;
     }
 
+    /**
+     * @dev called by the artist for them to get their money out of the contract!   
+     */
     function withdrawFee() public {
         uint256 amount = pendingWithdrawals[_msgSender()].mul(ARTIST_PERCENTAGE).div(100);
         require(amount > 0, "nothing to withdraw");
@@ -228,31 +190,23 @@ contract CreatorCollections is Ownable, Pausable {
     @dev sets a new contract as the newerVersionOfContract, if theres a newer contract address, you should use that.
     @param _newerContract the address of the newer version of this contract.  
     */
-    function setNewerContract(address _newerContract) public onlyOwner {
+    function setNewerContract(address _newerContract, string calldata versionName) public onlyOwner {
+        //TODO: make this use an emit event to let the frontend team know theres a newer version of this contract.
         newerVersionOfContract = _newerContract;
     }
 
     /**
-    @dev get the pools an address is the artist of, use this to get all the pool ids.
-    @param artist the address of the artist you want to find the pools of.
-    */
-    function getPoolsForArtist(address artist) public view returns (uint256[] memory) {
-        uint256[] memory accountsPools = accountToPools[artist];
-        return accountsPools;
-    }
-
-    /**
     @dev return the data for a FanCollectible and then get the money they have staked.
-    @param _pool the pool id
+    @param _catalog the catalog id
     @param _fanID the id of the FanCollectible you want to get the data for.
     @param _data the URI to the data for the FanCollectible.
     */
     function setFanCollectibleData(
-        uint256 _pool,
+        uint256 _catalog,
         uint256 _fanID,
         bytes memory _data
     ) public {
-        require(_msgSender() == pools[_pool].artist, "not the artist");
+        require(_msgSender() == catalogs[_catalog].artist, "not the artist");
 
         pendingWithdrawals[_msgSender()] = pendingWithdrawals[_msgSender()].add(heldBalances[_fanID].quantityHeld);
         heldBalances[_fanID].quantityHeld = 0;
--- contracts/test/CreatorCollections.test.ts
+++ contracts/test/CreatorCollections.test.ts
@@ -3,7 +3,6 @@ import { ethers,deployments } from "hardhat";
 import { Contract } from "@ethersproject/contracts";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { BigNumber, ContractFactory } from "ethers";
-import { Console } from "node:console";
 import { Test } from "mocha";
 
 
@@ -17,6 +16,7 @@ let CollectionsContract: Contract;
 let testingToken: ContractFactory;
 let testingTokenContract: Contract;
 let id;
+const now = Math.floor(Date.now()/1000) -50;
 describe('FanCollectible tokens', function(){
     beforeEach(async function(){
         [OwnerAccount,TestingAccount1,TestingAccount2] = await ethers.getSigners();
@@ -42,28 +42,19 @@ describe('CreatorCollections', function(){
         testingToken = await ethers.getContractFactory('TestingToken');
         testingTokenContract = await testingToken.deploy(6);
         CollectionsFactory = await ethers.getContractFactory('CreatorCollections');
-        CollectionsContract = await CollectionsFactory.deploy(OwnerAccount.address, CollectibleContract.address , testingTokenContract.address);
+        CollectionsContract = await CollectionsFactory.connect(OwnerAccount).deploy(CollectibleContract.address , testingTokenContract.address);
         await CollectibleContract.transferMinter(CollectionsContract.address);
 
     });
     it('Check initial supply', async function(){
         expect (await CollectionsContract.totalSupply()).to.equal(0);
     });
-    it('Create a pool', async function(){
-        id = (await CollectionsContract.connect(OwnerAccount).createPool(1,Math.floor(Date.now() / 1000), "My first collection"));
-       
-        const poolsArray = await CollectionsContract.getPoolsForArtist(OwnerAccount.address);
-        console.log(poolsArray);
-        expect (await poolsArray.length).to.greaterThan(0);
-    });
     it ('Testing Create Card', async function(){
-        id = (await CollectionsContract.connect(OwnerAccount).createPool(0, Math.floor(Date.now() / 1000), "My first collection"));
-        (await CollectionsContract.connect(OwnerAccount).createPool(1, Math.floor(Date.now() / 1000), "My second collection"));
-        const poolsArray = await CollectionsContract.getPoolsForArtist(OwnerAccount.address);
-        console.log(poolsArray);
+        id = (await CollectionsContract.connect(OwnerAccount).createCatalog(0, "My first collection"));
+        (await CollectionsContract.connect(OwnerAccount).createCatalog(1, "My second collection"));
 
-        const TokenId = await CollectionsContract.createCard(0, 10, ethers.utils.parseEther("10000"), Math.floor(Date.now() / 1000));
-        await CollectionsContract.createCard(1, 10, ethers.utils.parseEther("10000"), Math.floor(Date.now() / 1000));
+        const TokenId = await CollectionsContract.createCard(0, 10, ethers.utils.parseEther("10000"), now);
+        await CollectionsContract.createCard(1, 10, ethers.utils.parseEther("10000"), now);
 
         const cArray = await  CollectionsContract.getCardsArray(0);
         console.log(cArray);
@@ -86,22 +77,16 @@ describe('Purchasing single', function(){
         await testingTokenContract.connect(fanAccount).faucet();
 
         CollectionsFactory = await ethers.getContractFactory('CreatorCollections');
-        CollectionsContract = await CollectionsFactory.deploy(OwnerAccount.address, CollectibleContract.address , testingTokenContract.address);
+        
+        CollectionsContract = await CollectionsFactory.connect(OwnerAccount).deploy(CollectibleContract.address , testingTokenContract.address);
         await CollectibleContract.transferMinter(CollectionsContract.address);
     });
     it('Single Purchase', async function(){
         //where everything goes well!
         console.log(await (await artistAccount.getBalance()).toString());
         console.log(await fanAccount.getBalance().toString());
-        const id = (await CollectionsContract.connect(artistAccount).createPool(0, Math.floor(Date.now() / 1000), "My first collection"));
-        const cardID = await CollectionsContract.connect(artistAccount).createCard(0, 10, ethers.utils.parseEther("1"), Math.floor(Date.now() / 1000));
-        
-        //pretending that this is the web team writing this.
-        const artistPools = await CollectionsContract.getPoolsForArtist(artistAccount.address);
-        
-        
-
-        expect (await artistPools.length).to.equal(1);
+        const id = (await CollectionsContract.connect(artistAccount).createCatalog(0, "My first collection"));
+        const cardID = await CollectionsContract.connect(artistAccount).createCard(0, 10, ethers.utils.parseEther("1"), now);
 
         await testingTokenContract.connect(fanAccount).approve(CollectionsContract.address, ethers.utils.parseEther("10"));
         await CollectionsContract.connect(fanAccount).purchase(0, 0);
@@ -110,14 +95,14 @@ describe('Purchasing single', function(){
 
     });
     it('Single stake without funds', async function(){
-        const id = (await CollectionsContract.connect(artistAccount).createPool(0, Math.floor(Date.now() / 1000), "My first collection"));
-        const cardID = await CollectionsContract.connect(artistAccount).createCard(0, 10, ethers.utils.parseEther("1"), Math.floor(Date.now() / 1000));
+        const id = (await CollectionsContract.connect(artistAccount).createCatalog(0, "My first collection"));
+        const cardID = await CollectionsContract.connect(artistAccount).createCard(0, 10, ethers.utils.parseEther("1"), now);
 
         (await testingTokenContract.balanceOf(brokeAccount.address)/1e18).toLocaleString();
         expect(await testingTokenContract.balanceOf(brokeAccount.address)).to.equal(0);
 
         await testingTokenContract.connect(brokeAccount).approve(CollectionsContract.address, ethers.utils.parseEther("10"));
-        await expect(CollectionsContract.connect(brokeAccount).purchase(0, 0)).to.revertedWith("VM Exception while processing transaction: revert ERC20: transfer amount exceeds balance");
+        await expect(CollectionsContract.connect(brokeAccount).purchase(0, 0)).to.revertedWith("ERC20: transfer amount exceeds balance");
 
     });
 });
@@ -137,41 +122,41 @@ describe('Purchasing multiples', function(){
         await testingTokenContract.connect(fanAccount).faucet();
 
         CollectionsFactory = await ethers.getContractFactory('CreatorCollections');
-        CollectionsContract = await CollectionsFactory.deploy(OwnerAccount.address, CollectibleContract.address , testingTokenContract.address);
+        CollectionsContract = await CollectionsFactory.connect(OwnerAccount).deploy(CollectibleContract.address , testingTokenContract.address);
         await CollectibleContract.transferMinter(CollectionsContract.address);
     });
-    it('100 of 5 cards, 1 pool, 0 Purchased', async function(){
-        const poolId = await CollectionsContract.connect(artistAccount).createPool(1, Math.floor(Date.now() / 1000), "My first collection");
+    it('100 of 5 cards, 1 catalog, 0 Purchased', async function(){
+        const catalogId = await CollectionsContract.connect(artistAccount).createCatalog(1, "My first collection");
         const cardsIds = [];
         for(let i = 0; i < 5; i++){
-            cardsIds.push(await CollectionsContract.connect(artistAccount).createCard(1, 100, ethers.utils.parseEther("1"), Math.floor(Date.now() / 1000)));
+            cardsIds.push(await CollectionsContract.connect(artistAccount).createCard(1, 100, ethers.utils.parseEther("1"), now));
         }
         expect(cardsIds.length).to.equal(5);
         // expect(CollectionsContract.getCardsArray[1].length().should.equal(5));
     });
-    it('5 of 100 cards, 1 pool, 0 purchased', async function(){
-        const poolId = await CollectionsContract.connect(artistAccount).createPool(1, Math.floor(Date.now() / 1000), "My first collection");
+    it('5 of 100 cards, 1 catalog, 0 purchased', async function(){
+        const catalogId = await CollectionsContract.connect(artistAccount).createCatalog(1, "My first collection");
         const cardsIds = [];
         for(let i = 0; i < 100; i++){
-            cardsIds.push(await CollectionsContract.connect(artistAccount).createCard(1, 5, ethers.utils.parseEther("1"), Math.floor(Date.now() / 1000)));
+            cardsIds.push(await CollectionsContract.connect(artistAccount).createCard(1, 5, ethers.utils.parseEther("1"), now));
         }
         expect(cardsIds.length).to.equal(100);
     });
-    it('1 of 1 cards, 1 pool, 1 purchased', async function(){
-        const poolId = await CollectionsContract.connect(artistAccount).createPool(3, Math.floor(Date.now() / 1000), "My first collection");
+    it('1 of 1 cards, 1 catalog, 1 purchased', async function(){
+        const catalogId = await CollectionsContract.connect(artistAccount).createCatalog(3, "My first collection");
         const cardsIds = [];
         for(let i = 0; i < 1; i++){
-            cardsIds.push(await CollectionsContract.connect(artistAccount).createCard(3, 1, ethers.utils.parseEther("1"), Math.floor(Date.now() / 1000)));
+            cardsIds.push(await CollectionsContract.connect(artistAccount).createCard(3, 1, ethers.utils.parseEther("1"), now));
         }
         expect(cardsIds.length).to.equal(1);
         await testingTokenContract.connect(fanAccount).approve(CollectionsContract.address, ethers.utils.parseEther("10"));
         await CollectionsContract.connect(fanAccount).purchase(3, 0);
     });
-    it('5 of 5 cards, 1 pool, 2 purchased', async function(){
-        const poolId = await CollectionsContract.connect(artistAccount).createPool(3, Math.floor(Date.now() / 1000), "My first collection");
+    it('5 of 5 cards, 1 catalog, 2 purchased', async function(){
+        const catalogId = await CollectionsContract.connect(artistAccount).createCatalog(3, "My first collection");
         const cardsIds = [];
         for(let i = 0; i < 5; i++){
-            cardsIds.push(await CollectionsContract.connect(artistAccount).createCard(3, 5, ethers.utils.parseEther("1"), Math.floor(Date.now() / 1000)));
+            cardsIds.push(await CollectionsContract.connect(artistAccount).createCard(3, 5, ethers.utils.parseEther("1"), now));
         }
         expect(cardsIds.length).to.equal(5);
         await testingTokenContract.connect(fanAccount).approve(CollectionsContract.address, ethers.utils.parseEther("10"));
@@ -196,14 +181,14 @@ describe('Checking Payment to artist works correctly', function(){
         await testingTokenContract.connect(fanAccount).faucet();
 
         CollectionsFactory = await ethers.getContractFactory('CreatorCollections');
-        CollectionsContract = await CollectionsFactory.deploy(OwnerAccount.address, CollectibleContract.address , testingTokenContract.address);
+        CollectionsContract = await CollectionsFactory.connect(OwnerAccount).deploy(CollectibleContract.address , testingTokenContract.address);
         await CollectibleContract.transferMinter(CollectionsContract.address);
     });
     it('1 fulfilled purchase', async function(){
         let startingArtistBalance = await testingTokenContract.balanceOf(artistAccount.address);
         let startingFanBalance = await testingTokenContract.balanceOf(fanAccount.address);
-        await CollectionsContract.connect(artistAccount).createPool(3, Math.floor(Date.now() / 1000), "My first collection");
-        await CollectionsContract.connect(artistAccount).createCard(3, 5, ethers.utils.parseEther("1"), Math.floor(Date.now() / 1000));
+        await CollectionsContract.connect(artistAccount).createCatalog(3, "My first collection");
+        await CollectionsContract.connect(artistAccount).createCard(3, 5, ethers.utils.parseEther("1"), now);
         await testingTokenContract.connect(fanAccount).approve(CollectionsContract.address, ethers.utils.parseEther("10"));
         await CollectionsContract.connect(fanAccount).purchase(3, 0);
         expect(await testingTokenContract.balanceOf(fanAccount.address) < startingFanBalance);
