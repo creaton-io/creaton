23b22d778a539e28bace990771cdb8027a64c374
Aleix <nigeon@gmail.com>
2022-03-28 16:09:08 +0200

    Updated reaction Tokens deployment script

--- contracts/scripts/reactionTokens.ts
+++ contracts/scripts/reactionTokens.ts
@@ -1,5 +1,8 @@
+import { Contract } from "ethers";
 import { ethers, upgrades } from "hardhat";
 import { Address } from "hardhat-deploy/dist/types";
+
+const BICONOMY_FORWARDED_MUMBAI = "0x9399BB24DBB5C4b782C70c2969F58716Ebbd6a3b";
 
 const deployContract = async (
   contractName: string,
@@ -48,10 +51,10 @@ const deployReactionFactory = async () => {
     const sfResolver: Address = process.env.SUPERFLUID_RESOLVER || '';
     const sfVersion: string = process.env.SUPERFLUID_VERSION || '';
 
-    await deployContract(
+    return await deployContract(
         "ReactionFactory",
         "0xdF83f67321635C8c2Df962C0FB2ab9C8c92dBaB1",
-        [sfHost, sfCfa, sfSuperTokenFactory, sfResolver, sfVersion]
+        [sfHost, sfCfa, sfSuperTokenFactory, sfResolver, sfVersion, BICONOMY_FORWARDED_MUMBAI]
     );
 };
 
@@ -61,6 +64,24 @@ const upgradeReactionFactory = async (contractCurrentDeployedAddress: Address) =
         contractCurrentDeployedAddress,
     );
 
+const deployReactionToken = async(reactionFactoryAddress: Address) => {
+  const contractFactory = await ethers.getContractFactory("ReactionFactory");
+  const contract: Contract = contractFactory.attach(reactionFactoryAddress);
+
+  const reactionTokenName: string = 'Like';
+  const reactionTokenSymbol: string = 'LIKE';
+  const stakingTokenAddress: string = '0x0000000000000000000000000000000000000000';
+  const tokenMetadataURI: string = "https://gateway.pinata.cloud/ipfs/QmbVsqnwUrDJBBdbr1wjC4FZNKdN5i2jQoBJyGoea5bYy9";
+  const monthDistributionPercentage: number = 100;
+  let tx = await contract.deployReaction(reactionTokenName, reactionTokenSymbol, tokenMetadataURI, stakingTokenAddress, monthDistributionPercentage);
+  let receipt = await tx.wait();
+  receipt = receipt.events?.filter((x: any) => {return x.event == "ReactionDeployed"})[0];
+  return receipt.args;
+}
+
+  
 // Deployment
-deployReactionFactory().then(console.log).catch(console.error);
+// deployReactionFactory().then(console.log).catch(console.error);
 // upgradeReactionFactory().then(console.log).catch(console.error);
+
+deployReactionToken("0xECD17187d641dEe69Ff9417d072EC0C8DC36A2B1").then(console.log).catch(console.error);
