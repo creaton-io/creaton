f4f36ac01beb5835a85f8e8bce776d2b72916e92
Aleix <nigeon@gmail.com>
2022-04-19 13:17:48 +0200

    Moving all Config to .env files for the REACT APP. Documentation here: https://create-react-app.dev/docs/adding-custom-environment-variables/
    Also, added a section for feature flags in the config file.

--- react-app/src/Config.tsx
+++ react-app/src/Config.tsx
@@ -1,71 +1,40 @@
-let APOLLO_URI,
-  REENCRYPTION_URI,
-  REACTION_CONTRACT_ADDRESS,
-  REACTION_ERC20,
-  CREATE_TOKEN_ADDRESS,
-  GOVERNANCE_SQUAD_TOKENS,
-  BICONOMY_API,
-  BICONOMY_AUTH,
-  VOTING_GRAPHQL_URI,
-  CREATOR_VOTING_ADDRESS;
-const ARWEAVE_GATEWAY = 'https://arweave.net/';
-const REPORT_URI = 'https://report.creaton.io/report';
-const ARWEAVE_URI = 'https://arweave.creaton.io';
-const FAUCET_URI = 'https://faucet.creaton.io/give-me-some';
-if (process.env.NODE_ENV === 'development') {
-  REENCRYPTION_URI = 'https://staging.creaton.io';
-  // APOLLO_URI = 'http://api.graph.io:8000/subgraphs/name/creaton-io/creaton'
-  APOLLO_URI = 'https://api.thegraph.com/subgraphs/name/creaton-io/creaton-dev';
+export const ARWEAVE_GATEWAY = process.env.REACT_APP_ARWEAVE_GATEWAY;
+export const REPORT_URI = process.env.REACT_APP_REPORT_URI;
+export const ARWEAVE_URI = process.env.REACT_APP_ARWEAVE_URI;
+export const FAUCET_URI = process.env.REACT_APP_FAUCET_URI;
 
-  BICONOMY_API = process.env.BICONOMY_API;
-  BICONOMY_AUTH = process.env.BICONOMY_AUTH;
+export const REENCRYPTION_URI = process.env.REACT_APP_REENCRYPTION_URI;
+export const APOLLO_URI = process.env.REACT_APP_SUBGRAPH_URI;
 
-  REACTION_CONTRACT_ADDRESS = '0xd57d3d31a7b3ae7ac1400da47f13cdb9c6522e70';
-  REACTION_ERC20 = '0xe2ee5f719a12a85dc7cdeb04fad3ebc0ffe185de';
-  CREATE_TOKEN_ADDRESS = '0xe2ee5f719a12a85dc7cdeb04fad3ebc0ffe185de';
-  GOVERNANCE_SQUAD_TOKENS = {
-    GOV: '0x21f551FBA148f36fA369601A8eD0D7e3Ad6708ee',
-    MKT: '0xb339165C55C3F8BE35033CB26c2505f8B0912C26',
-    DEV: '0x06805b2b1a5ab9f6f753f8d220cccef006d1cf8e',
-  };
-  VOTING_GRAPHQL_URI = 'https://api.studio.thegraph.com/query/2670/creator-voting/v0.0.21'
-  CREATOR_VOTING_ADDRESS = '0x9DAEb40970A95C817C1f37EBB765d660fDE0ff64';
-} else if (process.env.NODE_ENV === 'production') {
-  //staging
-  REENCRYPTION_URI = 'https://reencryption.creaton.io';
-  APOLLO_URI = 'https://api.thegraph.com/subgraphs/name/creaton-io/creaton-polygon'; //'https://api.thegraph.com/subgraphs/name/creaton-io/creaton-dao'
+export const BICONOMY_API = process.env.BICONOMY_API;
+// BICONOMY_AUTH SHOULDN'T BE HERE!!! THIS SHOULD BE PRIVATE!!!!
+export const BICONOMY_AUTH = process.env.BICONOMY_AUTH;
+// BICONOMY_AUTH SHOULDN'T BE HERE!!! THIS SHOULD BE PRIVATE!!!!
 
-  BICONOMY_API = process.env.BICONOMY_API;
-  BICONOMY_AUTH = process.env.BICONOMY_AUTH;
-}
-//@ts-ignore
-else if (process.env.NODE_ENV === 'staging') {
-  //staging
-  REENCRYPTION_URI = 'https://reencryption.creaton.io';
-  APOLLO_URI = 'https://api.thegraph.com/subgraphs/name/creaton-io/creaton-mumbai'; //'https://api.thegraph.com/subgraphs/name/creaton-io/creaton-dao'
+export const REACTION_CONTRACT_ADDRESS = process.env.REACT_APP_REACTION_CONTRACT_ADDRESS;
+export const REACTION_ERC20 = process.env.REACT_APP_REACTION_ERC20;
+export const CREATE_TOKEN_ADDRESS = process.env.REACT_APP_CREATE_TOKEN_ADDRESS;
 
-  BICONOMY_API = process.env.BICONOMY_API;
-  BICONOMY_AUTH = process.env.BICONOMY_AUTH;
-}
-//TODO:PRODUCTION CONFIG
-// {
-//   REENCRYPTION_URI = 'https://reencryption.creaton.io'
-//   APOLLO_URI = 'https://api.thegraph.com/subgraphs/name/creaton-io/creaton'
-// }
+export const GOVERNANCE_SQUAD_TOKENS = {
+  GOV: process.env.REACT_APP_GOVERNANCE_SQUAD_TOKENS_GOV,
+  MKT: process.env.REACT_APP_GOVERNANCE_SQUAD_TOKENS_MKT,
+  DEV: process.env.REACT_APP_GOVERNANCE_SQUAD_TOKENS_DEV,
+};
+export const VOTING_GRAPHQL_URI = process.env.REACT_APP_VOTING_GRAPHQL_URI;
+export const CREATOR_VOTING_ADDRESS = process.env.REACT_APP_CREATOR_VOTING_ADDRESS;
+
+/**
+ * FEATURE FLAGS
+ * We might need more logic
+ */
+let NFTLANCE_ENABLED = false;
+let REACTION_TOKENS_ENABLED = false;
+let CREATON_VOTING_ENABLED = false;
+let MODERATION_ENABLED = false;
 
 export {
-  APOLLO_URI,
-  ARWEAVE_URI,
-  ARWEAVE_GATEWAY,
-  REENCRYPTION_URI,
-  REPORT_URI,
-  FAUCET_URI,
-  REACTION_CONTRACT_ADDRESS,
-  REACTION_ERC20,
-  CREATE_TOKEN_ADDRESS,
-  GOVERNANCE_SQUAD_TOKENS,
-  BICONOMY_API,
-  BICONOMY_AUTH,
-  VOTING_GRAPHQL_URI,
-  CREATOR_VOTING_ADDRESS,
+  NFTLANCE_ENABLED,
+  REACTION_TOKENS_ENABLED,
+  CREATON_VOTING_ENABLED,
+  MODERATION_ENABLED
 };
