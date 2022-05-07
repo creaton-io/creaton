6ba8adec3ae6e52aa70ffc5e538992bfe4fafe1a
Aeroxander <alexanderpklus@gmail.com>
2022-05-07 17:32:27 +0200

    new build

--- react-app/src/connectors/magic.ts
+++ react-app/src/connectors/magic.ts
@@ -2,8 +2,8 @@ import { initializeConnector } from '@web3-react/core'
 import { Magic } from '@web3-react/magic'
 
 const customNodeOptions = {
-  rpcUrl: 'https://rpc-mumbai.matic.today/', // Polygon RPC URL
-  chainId: 80001 // Polygon chain id
+  rpcUrl: 'https://polygon-rpc.com', // Polygon RPC URL
+  chainId: 137 // Polygon chain id
 }
 
 export const [magic, hooks] = initializeConnector<Magic>((actions) => new Magic(actions, {apiKey: 'pk_live_55D93A0BD91B3D6E', network: customNodeOptions}));
