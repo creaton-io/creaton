65544569d2429955a3142b8009f32ccbb780dc99
Jay | BizWeb2/3Tips <37313982+futuristfuturist@users.noreply.github.com>
2022-05-09 17:04:11 +0200

    Update Creators.tsx
    Update Creators.tsx

--- react-app/src/Creators.tsx
+++ react-app/src/Creators.tsx
@@ -1,9 +1,17 @@
+/* ****************** IMPORTS ****************** */
+
 import {gql, useQuery} from '@apollo/client';
 import React, {useEffect} from 'react';
 import {FilterList} from './components/filter-list';
 import {useWeb3React} from '@web3-react/core';
 import {Web3Provider} from '@ethersproject/providers';
 import {Splash} from './components/splash';
+
+
+
+
+
+/* ****************** GRAPH QUERIES ****************** */
 
 const CREATORS_QUERY = gql`
   query {
@@ -26,11 +34,24 @@ const CREATORS_QUERY = gql`
   }
 `;
 
+
+
+
+
+/* ****************** LOADING, ERROR ****************** */
+
 function Creators() {
   const {loading, error, data} = useQuery(CREATORS_QUERY, {pollInterval: 10000});
   //const {account} = useWeb3React<Web3Provider>()
   if (loading) return <Splash src="https://assets5.lottiefiles.com/packages/lf20_bkmfzg9t.json"></Splash>;
   if (error) return <p>Error :(</p>;
+  
+
+
+
+
+/* ****************** DEFINE LIST ****************** */
+  
   const items = data.creators.map((creator: any) => {
     // let subtitle = '$' + creator.subscriptionPrice + ' / month'
     // if(account){
@@ -40,6 +61,13 @@ function Creators() {
     //       subtitle="Subscribed"
     //   }
     // }
+
+
+
+
+
+/* ****************** PROFILE DATA ****************** */
+
     return {
       avatar: creator.profile !== null ? JSON.parse(creator.profile.data).image : '',
       title: creator.profile !== null ? JSON.parse(creator.profile.data).username : creator.id.slice(0, 6),
@@ -51,8 +79,21 @@ function Creators() {
       creatorAddress: creator.id,
     };
   });
+
+
+
+
+
+/* ****************** LIST OF PROFILES ****************** */
+
   return <FilterList list={items} />;
 }
+
+
+
+
+
+/* ****************** EXPORT ****************** */
 
 export {CREATORS_QUERY};
 export default Creators;
